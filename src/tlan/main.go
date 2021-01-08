4967402f8a5082c101491a9bf3c7270b0fd2a773
Marcos Silveira <bymarkone@gmail.com>
2021-01-08 12:57:40 +0100

    Refactor repl, show help

--- src/tlan/main.go
+++ src/tlan/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/user"
+	"tlan/parser"
 	"tlan/repl"
+	"io/ioutil"
+	"tlan/interpreter"
 )
+
+const DATA_FOLDER = "./../../data"
 
 func main() {
 	user, err := user.Current()
@@ -14,6 +20,29 @@ func main() {
 		panic(err)
 	}
 
+	load()
 	fmt.Printf("Hello %s! Welcome to tlan\n", user.Username)
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func load() {
+	fmt.Printf("Loading data2... \n")
+	filesInfo, err := ioutil.ReadDir(DATA_FOLDER)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for _, file := range filesInfo {
+		fmt.Printf("Processing file %s \n", file.Name())
+		content, err := ioutil.ReadFile(DATA_FOLDER + "/" + file.Name())
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		text := string(content)
+		l := parser.NewLexer(text)
+		p := parser.NewParser(l)
+		items := p.Parse()
+		interpreter.Eval("project", items)
+	}
 }
--- src/tlan/repl/repl.go
+++ src/tlan/repl/repl.go
@@ -4,43 +4,90 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os/exec"
 	"strings"
+	"tlan/interpreter"
 )
 
 const PROMPT = ">> "
+var out io.Writer
 
-func Start(in io.Reader, out io.Writer) {
+func Start(in io.Reader, _out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	out = _out
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		_, _ = fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
-
-		if strings.HasPrefix(line, "show") {
-			words := strings.Split(line, " ")
-			if len(words) == 1 {
-				fmt.Print("Incorrect number of arguments\n")
-				continue
-			}
-			switch words[1] {
-			case "schedule":
-				printSchedule()
-			case "plan":
-				printPlan()
-			}
-		} else if strings.ToLower(strings.TrimSpace(line)) == "exit" {
-			break
-		} else if strings.ToLower(strings.TrimSpace(line)) == "now" {
-			fmt.Printf("Hello \n")
+		words := strings.Split(line, " ")
+		if len(words) == 0 {
+			continue
 		}
 
-
+		switch words[0] {
+		case "show":
+			show(words)
+		case "help":
+			help(words)
+		case "clear":
+			clear()
+		case "exit":
+			break
+		case "now":
+			now(words)
+		}
 	}
+}
+
+func help(words []string) {
+	if len(words) == 1 {
+		printHelp()
+		return
+	}
+
+	switch words[1] {
+	case "show":
+		printShowHelp()
+	}
+}
+
+func show(words []string) {
+	if len(words) == 1 {
+		fmt.Print("Incorrect number of arguments. Type 'help show' to see usage.\n")
+		return
+	}
+	switch words[1] {
+	case "schedule":
+		printSchedule()
+	case "plan":
+		printPlan()
+	case "projects":
+		printProjects()
+	}
+}
+
+func now(words []string) {
+
+}
+
+func clear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = out
+	_ = cmd.Run()
+}
+
+func printProjects() {
+	projects := interpreter.ListProjects()
+	fmt.Print("\nListing projects: \n\n")
+	for _, project := range projects {
+		fmt.Printf("- %s\n", project.Name)
+	}
+	fmt.Print("\n\n")
 }
 
 func printPlan() {
@@ -54,4 +101,30 @@ func printSchedule() {
 
 func printPipeline() {
 
+}
+
+func printShowHelp() {
+	printEmptyLine()
+	printTlanHeader()
+	_, _ = fmt.Fprint(out, "'show' usage\n")
+	printEmptyLine()
+}
+
+func printHelp() {
+	printEmptyLine()
+	printTlanHeader()
+	_, _ = fmt.Fprint(out, "Commands:\n")
+	_, _ = fmt.Fprint(out, "  help [command]              : prints help information for commands\n")
+	_, _ = fmt.Fprint(out, "  show projects|tracks|plan   : prints a specific view for each object\n")
+	_, _ = fmt.Fprint(out, "  now                         : shows tasks to be performed now (i.e the current time slot)\n")
+	_, _ = fmt.Fprint(out, "  exit                        : exits the application\n")
+	printEmptyLine()
+}
+
+func printEmptyLine() {
+	_, _ = fmt.Fprint(out, "\n")
+}
+
+func printTlanHeader() {
+	_, _ = fmt.Fprint(out, "tLan - a language for time\n")
 }
